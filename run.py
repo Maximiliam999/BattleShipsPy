21cdb9f725f01400e47782be2c9fa6cecffb9164
Maximiliam999 <maximanuel777@gmail.com>
2023-01-24 19:26:34 +0000

    Remove all old  and put in new functions

--- run.py
+++ run.py
@@ -1,10 +1,27 @@
-from random import randint 
+HIDDEN_BOARD = [[' '] * 5 for x in range(5)]
+GUESS_BOARD = [[' '] * 5 for x in range(5)]
 
-board = []
-
-for x in range(5):
-    board.append(['*'] * 5)
 
 def print_board(board):
+    row_number = 1
     for row in board:
-        print((' ').join(row))
+        print('%d|%s' % (row_number, "|".join(row)))
+        row_number += 1      
+
+
+
+def create_ships(board):
+    for ship in range(4):
+        ship_column, ship_row = randint(0, 4), randint(0, 4)
+        while board[ship_row][ship_column] == "X":
+            ship_column, ship_row = randint(0, 4), randint(0, 4)
+        board[ship_row][ship_column] = 'X'
+       
+
+
+def Guess():
+    column = input('Guess between column numbers 1-5')
+    row = input('Guess between row numbers 1-5')
+
+def Run_Game():
+
